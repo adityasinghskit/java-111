367b2fb13503b1c72c1fffa96888acd9121414b2
adityasinghskit <adityasinghskit@gmail.com>
2022-05-23 16:09:42 +0530

    added U4C1-3

--- DailyAssignments/U4C1/src/q4/Main.java
+++ DailyAssignments/U4C1/src/q4/Main.java
@@ -1,5 +1,26 @@
 package q4;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Number of student to add: ");
+	int n=scan.nextInt();
+	for(int i=1;i<=n;i++) {
+		Student s1=new Student();
+		System.out.println("Roll Number:");
+		s1.setR(scan.nextInt());
+		System.out.println("Name:");
+		s1.setN(scan.next());
+		System.out.println("Marks:");
+		s1.setM(scan.nextInt());
+		System.out.println("Student"+i+"details");
+		System.out.println("Student roll no: "+s1.getR());
+		System.out.println("Student name: "+s1.getN());
+		System.out.println("Student marks: "+s1.getM());
+	}
+	scan.close();
+}
 }
--- DailyAssignments/U4C1/src/q4/Student.java
+++ DailyAssignments/U4C1/src/q4/Student.java
@@ -1,5 +1,35 @@
 package q4;
 
 public class Student {
-
+private int rollNumber;
+private String studentName;
+private int marks;
+Student(){
+	
+}
+Student(int roll, String name, int marks){
+	this.rollNumber=roll;
+	this.studentName=name;
+	this.marks=marks;
+}
+//getter
+public int getR() {
+	return this.rollNumber;
+}
+public String getN() {
+	return this.studentName;
+}
+public int getM() {
+	return this.marks;
+}
+//setter
+public void setR(int r) {
+	this.rollNumber=r;
+}
+public void setN(String n) {
+	this.studentName=n;
+}
+public void setM(int m) {
+	this.marks=m;
+}
 }
