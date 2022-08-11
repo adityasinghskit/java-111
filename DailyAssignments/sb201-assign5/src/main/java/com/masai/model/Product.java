a11e948e5df44b92c6dbed301c676879331acca9
adityasinghskit <adityasinghskit@gmail.com>
2022-08-11 22:55:25 +0530

    updated sb201-assign5

--- DailyAssignments/sb201-assign5/src/main/java/com/masai/model/Product.java
+++ DailyAssignments/sb201-assign5/src/main/java/com/masai/model/Product.java
@@ -19,10 +19,15 @@ public class Product {
 private Integer id;
 @Size(min=3, max=16, message="{name.invalid}")
 private String name;
+@Min(value=1, message="{id.invaid}")
 private Integer price;
+@Size(min=3, max=16, message="{name.invalid}")
 private String brand;
+@Size(min=3, max=16, message="{name.invalid}")
 private String category;
+@Min(value=1, message="{id.invaid}")
 private Integer registration_number;
+@Min(value=1, message="{id.invaid}")
 private Integer manufacturing_id;
 
 
